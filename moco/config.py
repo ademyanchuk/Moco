2688e6bdda43c64a2af564d9b10ec083780535b9
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2021-02-26 13:01:49 +0100

    Exp v1 (close to paper h-params)

--- moco/config.py
+++ moco/config.py
@@ -3,31 +3,26 @@ from pathlib import Path
 
 import yaml
 
+from .optimizer import sgd_conf, cosine_conf
+
 DATA_ROOT = Path("~/Data/NIHChest").expanduser()
 PROJ_PATH = Path("~/Projects/Moco").expanduser()
 LOGS_PATH = PROJ_PATH / "logs"
 MODELS_PATH = PROJ_PATH / "models"
 
 Config = {
-    "debug": True,
+    "debug": False,
     "resume": "",  # empty string or exisiting experiment name
-    "crop_size": 256,
-    "batch_size": 16,
+    "crop_size": 320,
+    "batch_size": 32,
     "moco_dim": 128,
     "moco_K": 4096,
     "moco_m": 0.999,
-    "moco_T": 0.1,
-    "moco_arch": "resnet50d",
-    "opt_conf": {"adamw": {"lr": 5e-4, "weight_decay": 0.0}},
-    "sch_conf": {
-        "cosine": {
-            "t_initial": 2,
-            "lr_min": 5e-8,
-            "warmup_t": 0,
-            "warmup_lr_init": 5e-7,
-        }
-    },
-    "num_epochs": 2,
+    "moco_T": 0.2,
+    "moco_arch": "resnet200d",
+    "opt_conf": sgd_conf,
+    "sch_conf": cosine_conf,
+    "num_epochs": 290,
 }
 
 
